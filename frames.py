8c64a35bab7ec3dbaca8e20dfc6e7c8ef5132d6b
omar <omar.csse@gmail.com>
2019-08-12 18:20:43 +1000

    add frames.py

--- frames.py
+++ frames.py
@@ -8,66 +8,88 @@ from os import listdir
 
 class Frames():
 
-    def __init__(self, start_number=1, cap_pernumber_frame=5):
+    """Frames model to capture specified number of frames in videos."""
 
+    def __init__(self, start_number=1):
         self._dir_path = os.path.dirname(os.path.realpath(__file__))
-        self.start_number = start_number
-        self.cap_pernumber_frame = cap_pernumber_frame
+        self._log_file = None
+        self._dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    def _cleanDirectories(self, clean_logs=False):
+        if os.path.exists(self._dir_path + '/logs/log.txt'):
+            os.remove(self._dir_path + '/logs/log.txt')
+
+    def openLogs(self):
+        self._cleanDirectories()
+        if not os.path.exists(self._dir_path+'/logs'): 
+            os.makedirs(self._dir_path+'/logs')
+        self._log_file = open(self._dir_path + '/logs/log.txt', 'a')
+        return self._log_file
+
+    def closeLogs(self):
+        self._log_file.close()
 
     def setup(self, objectType='surfers'):
 
         if not os.path.exists(self._dir_path+'/frames'): 
             os.mkdir(self._dir_path+'/frames')
+        
+        self._log_file.write("""Frames model to capture specified number of frames in videos.""")
+        self._log_file.write("\n\n\n")
+        if not os.path.exists(self._dir_path+'/frames/' + objectType): 
+            os.mkdir(self._dir_path+'/frames/'+objectType)
 
         self._frames_folder = self._dir_path + '/frames/' + objectType
         self.object_dir = self._dir_path + '/videos/' + objectType
         self.objectVideos = [self.object_dir+'/'+f for f in listdir(self.object_dir)]
 
-    def capture_frames(self):
+    def capture_frames(self, cap_per_frame=5):
 
         for video in self.objectVideos:
 
-            print("\n\n{}".format(os.path.basename(video)))
-            print(self._frames_folder)
+            if not os.path.exists(self._frames_folder + '/' + os.path.basename(video)): 
+                os.mkdir(self._frames_folder + '/' + os.path.basename(video))
             
             video_reader = cv2.VideoCapture(video)
+            total_caps = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+            fps = int(video_reader.get(cv2.CAP_PROP_FPS))
 
-            fps = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
-            start_frame = sum(x * int(t) for x, t in zip([3600, 60, 1], start_time.split(":"))) * 25
-            end_frame = sum(x * int(t) for x, t in zip([3600, 60, 1], end_time.split(":")))  * 25
-            nb_caps = (end_frame - start_frame) # cap_pernumber_frame
-            count = 0
-            cap_count = start_number
-            print("\n\n{}".format(os.path.basename(video)))
-            print('Number of frames to process: {}'.format(fps))
-            print('Number of frames to capture: {}\n\n'.format(nb_caps))
+            if cap_per_frame is 'min': cap_per_frame = fps
+            elif cap_per_frame is 'max': cap_per_frame = 1
 
-            # for i in range(fps):
-            #     ret, image = video_reader.read()
-            #     if i > start_frame:
-            #         count = count+1
-            #         if count % self.cap_pernumber_frame == 0:
-            #             out_image = cv2.resize(image, (3840, 2160))
-            #             cv2.imwrite('{}/{}/{}.png'.format(self._frames_folder, os.path.basename(video), cap_count), out_image)
-            #             #replace test{} by Dolphin{} etc.caps by dir you want
-            #             print('Saving capture #{} for {}'.format(cap_count, os.path.basename(video)))
-            #             cap_count = cap_count+1
-
-            #     if i == end_frame:
-            #         print('job done')
-            #         break
+            cap_count = 0
+            print("\n{}\n\n".format(os.path.basename(video)))
+            self._log_file.write("\nVideo name: {}\n\n".format(os.path.basename(video)))
+            print('Number of frames to process: {}'.format(video_reader.get(cv2.CAP_PROP_FRAME_COUNT)))
+            self._log_file.write('Number of frames to process: {}\n'.format(video_reader.get(cv2.CAP_PROP_FRAME_COUNT)))
+            print('Number of fps to capture: {}\n\n'.format(fps))
+            self._log_file.write('Number of fps to capture: {}\n\n'.format(fps))
+            
+            for i in range(total_caps):
+                ret, image = video_reader.read()
+                if i % cap_per_frame == 0:
+                    cv2.imwrite('{}/{}/{}.png'.format(self._frames_folder, os.path.basename(video), cap_count), image)
+                    print('frame#:{} - captur: #{} for {}'.format(i, cap_count, os.path.basename(video)))
+                    self._log_file.write('frame#:{} - captur: #{} for {}\n'.format(i, cap_count, os.path.basename(video)))
+                    cap_count = cap_count + 1
                         
-            # video_reader.release()
+            video_reader.release()
+            self._log_file.write("\n\n")
+            print("\n\n")
 
 
 def main():
-    frames = Frames(start_number=1, cap_pernumber_frame=5)
+    frames = Frames(start_number=0)
+    logs_file = frames.openLogs()
 
     frames.setup(objectType='surfers')
-    frames.capture_frames()
+    frames.capture_frames(cap_per_frame='min')
 
     frames.setup(objectType='sharks')
-    frames.capture_frames()
+    frames.capture_frames(cap_per_frame=5)
+
+    logs_file.write('\n\nCapturing frames in all videos have been done')
+    frames.closeLogs()
 
 if __name__ == "__main__":
     main()
