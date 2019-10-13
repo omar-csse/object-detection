ffaafde57d5bd5f36a9a16000598008fdc0ed4b7
omar <omar.csse@gmail.com>
2019-10-14 09:05:59 +1000

    check for nan in csv

--- gui/playback.py
+++ gui/playback.py
@@ -59,7 +59,8 @@ class PlayBack(QThread):
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         for index, row in item.iterrows():
             for i in range(0, expandedData.shape[1], 6):
-                if expandedData[i][index] == None or not expandedData[i][index]:
+                print(expandedData[i][index])
+                if expandedData[i][index] == None or not expandedData[i][index] or pd.isnull(expandedData[i][index]):
                     break # it breaks the loop if there are no more objects to draw
                 colour = self.colours[self.classes.index(expandedData[i][index])]
                 objClass = expandedData[i][index]
